c611e77e7aa1d79abf8e24594d276c8f686723c9
Erik Hetzner <ehetzner@gmail.com>
2010-06-10 13:54:41 -0700

    add RankedWebGraph class for calculating PR from stored bvgraph

---
+++ src/org/cdlib/was/ngIndexer/RankedWebGraph.scala
@@ -0,0 +1,48 @@
+package org.cdlib.was.ngIndexer;
+
+import it.unimi.dsi.law.rank._;
+import it.unimi.dsi.webgraph._;
+
+class RankedWebGraph (basename : String) extends WebGraph {
+  val bvg : BVGraph = BVGraph.load(basename);
+  val urlFile = "%s.urls".format(bvg.basename);
+
+  def addLink (link : Outlink) = ();
+  def addLinks (links : Seq[Outlink]) = ();
+
+  lazy val urls = scala.io.Source.fromFile(urlFile).getLines.toList.toArray;
+
+  lazy val ranks = {
+    val pr = new PageRankPowerMethod(bvg);
+    pr.init;
+    pr.stepUntil(new PageRank.NormDeltaStoppingCriterion(PageRank.DEFAULT_THRESHOLD));
+    pr.rank;
+  }
+
+  def numNodes = bvg.numNodes;
+
+  class RankedNodeIterator extends NodeIterator {
+    var position = -1;
+    val underlying = bvg.nodeIterator();
+    def url = urls(position);
+    
+    override def outdegree = underlying.outdegree;
+
+    override def hasNext = underlying.hasNext;
+    
+    override def successorArray = underlying.successorArray;
+
+    override def nextInt : Int = next.asInstanceOf[Int];
+      
+    override def next : java.lang.Integer = {
+      position = underlying.nextInt;
+      return position;
+    }
+
+    def rank = ranks(position);
+
+    def boost = ranks(position) * numNodes;
+  }
+
+  override def nodeIterator = new RankedNodeIterator();
+}
