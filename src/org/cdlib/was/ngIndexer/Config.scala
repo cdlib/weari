e7def204a2ba269c5b9ee8751bdd654422528539
Erik Hetzner <erik.hetzner@ucop.edu>
2010-07-26 18:05:38 -0700

    Add Outlink, Config classes in separate files.

---
+++ src/org/cdlib/was/ngIndexer/Config.scala
@@ -0,0 +1,7 @@
+package org.cdlib.was.ngIndexer;
+
+class Config {
+  var indexer : SolrIndexer = null;
+  def setIndexer (indexer : SolrIndexer) = this.indexer = indexer;
+}
+    
---
+++ src/org/cdlib/was/ngIndexer/Outlink.scala
@@ -0,0 +1,9 @@
+package org.cdlib.was.ngIndexer;
+
+import java.util.Date;
+import org.archive.net.UURI;
+
+class Outlink (val from : UURI,
+               val to : UURI,
+               val date : Date,
+               val text : String) {}
