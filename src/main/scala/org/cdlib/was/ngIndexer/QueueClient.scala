a79a614a0bd2cddb1e25e3d533604c1087fba0fd
Erik Hetzner <erik.hetzner@ucop.edu>
2011-01-31 16:06:56 -0800

    add dead simple queue client

---
+++ src/main/scala/org/cdlib/was/ngIndexer/QueueClient.scala
@@ -0,0 +1,22 @@
+package org.cdlib.was.ngIndexer;
+
+import org.apache.zookeeper.recipes.queue.DistributedQueue;
+import org.apache.zookeeper.recipes.queue.Item;
+import org.apache.zookeeper.ZooKeeper;
+
+object QueueClient {
+  def main (args : Array[String]) {
+    if (args.size < 2) {
+      System.err.println("Usage: ... ZOOKEEEPER_HOSTS QUEUE_NAME");
+      System.exit(1);
+    }
+    var zookeeper = new ZooKeeper(args(0), 10000, 
+                                  new DistributedQueue.Ignorer());
+    val q = new DistributedQueue(zookeeper, args(1), null);
+    var nextCommand = readLine();
+    while (nextCommand != null) {
+      q.submit(nextCommand.getBytes("UTF-8"));
+      nextCommand = readLine();
+    }
+  }
+}
