5d7531ebbcbc0fd4833c9d4132cac0a03d05e552
Erik Hetzner <erik.hetzner@ucop.edu>
2011-02-07 17:42:56 -0800

    adding support for warc records

--- src/main/scala/org/cdlib/was/ngIndexer/SolrProcessor.scala
+++ src/main/scala/org/cdlib/was/ngIndexer/SolrProcessor.scala
@@ -15,6 +15,7 @@ import org.apache.tika.parser.{AutoDetectParser, ParseContext, Parser}
 
 import org.archive.io.ArchiveRecord
 import org.archive.io.arc.ARCRecord
+import org.archive.io.warc.WARCRecord
 import org.archive.net.UURIFactory
 
 import org.cdlib.was.ngIndexer.webgraph.WebGraphContentHandler
@@ -129,67 +130,81 @@ class SolrProcessor {
   val MIN_BOOST = 0.1f;
   val MAX_BOOST = 10.0f;
 
+  /** Cue up a record to it's start, & return the content type. */
+  def readyRecord (archiveRecord : ArchiveRecord) : Option[String] = {
+    archiveRecord match {
+      case rec : WARCRecord => {
+        Utility.parseHeaders(rec) match {
+          case None => return None;
+          case Some((responseCode, headers)) => {
+            return Some((headers.get(HttpHeaders.CONTENT_TYPE.toLowerCase).get.getValue));
+          }
+        }
+      }
+      case rec : ARCRecord => {
+        Utility.skipHttpHeader(rec);
+        return Some(rec.getMetaData.getMimetype.toLowerCase);
+      }
+    }
+  }
+
   /** Take an archive record & return a solr document.
     *
     */
-  def record2doc(archiveRecord : ArchiveRecord) : Option[SolrInputDocument] = {
-    archiveRecord match {
-      case rec : ARCRecord => {
-        Utility.skipHttpHeader(rec);
-        val tikaMetadata = new Metadata();
-        val parseContext = new ParseContext();
-        val url = rec.getMetaData.getUrl;
-        val recHeader = rec.getHeader;
-        val contentType = rec.getMetaData.getMimetype.toLowerCase;
-        val doc = new SolrInputDocument();
-        val indexContentHandler = new NgIndexerContentHandler(rec.getHeader.getLength  >= 1048576);
-        val wgContentHandler = new WebGraphContentHandler(url, rec.getHeader.getDate);
-        val contentHandler = new MultiContentHander(List[ContentHandler](wgContentHandler, indexContentHandler));
-        val detector = (new TikaConfig).getMimeRepository;
-        val parser = new AutoDetectParser(detector);
-        parseContext.set(classOf[Parser], parser);
+  def record2doc(rec : ArchiveRecord) : Option[SolrInputDocument] = {
+    val contentType = readyRecord(rec);
+    if (contentType.isEmpty) { return None; }
+    val tikaMetadata = new Metadata();
+    val parseContext = new ParseContext();
+    val recHeader = rec.getHeader;
+    val url = recHeader.getUrl;
+    val doc = new SolrInputDocument();
+    val indexContentHandler = new NgIndexerContentHandler(rec.getHeader.getLength  >= 1048576);
+    val wgContentHandler = new WebGraphContentHandler(url, rec.getHeader.getDate);
+    val contentHandler = new MultiContentHander(List[ContentHandler](wgContentHandler, indexContentHandler));
+    val detector = (new TikaConfig).getMimeRepository;
+    val parser = new AutoDetectParser(detector);
+    parseContext.set(classOf[Parser], parser);
 
-        tikaMetadata.set(HttpHeaders.CONTENT_LOCATION, url);
-        tikaMetadata.set(HttpHeaders.CONTENT_TYPE, contentType);
-        try {
-          try {
-            parser.parse(rec, contentHandler, tikaMetadata, parseContext);
-          } catch {
-            case ex : Throwable => {
-              logger.error("Error reading {}", rec.getHeader.getUrl, ex);
-            }
-          }
-          /* finish index */
-          rec.close;
-          indexContentHandler.contentString.map(str=>doc.addField(CONTENT_FIELD, str));
-
-          val title = tikaMetadata.get("title") match {
-            case s : String => s
-            case null => ""
-          }
-          updateDocBoost(doc, 1.0f);
-          updateDocUrlDigest(doc, url, rec.getDigestStr);
-          doc.addField(DATE_FIELD, recHeader.getDate.toLowerCase, 1.0f);
-          doc.addField(TYPE_FIELD, tikaMetadata.get(HttpHeaders.CONTENT_TYPE), 1.0f);
-          doc.addField(TITLE_FIELD, title, 1.0f);
-          doc.addField(CONTENT_LENGTH_FIELD, recHeader.getLength, 1.0f);
-          /* finish webgraph */
-          if (webGraphTypeRE.matcher(tikaMetadata.get(HttpHeaders.CONTENT_TYPE)).matches) {
-            val outlinks = wgContentHandler.outlinks;
-            if (outlinks.size > 0) {
-              val outlinkFps = for (l <- outlinks) 
-                               yield UriUtils.fingerprint(l.to);
-              for (fp <- outlinkFps.toList.distinct.sortWith((a,b)=>(a < b))) {
-                doc.addField("outlinks", fp);
-              }
-            }
-          }
-          return Some(doc);
+    tikaMetadata.set(HttpHeaders.CONTENT_LOCATION, url);
+    tikaMetadata.set(HttpHeaders.CONTENT_TYPE, contentType.get);
+    try {
+      try {
+        parser.parse(rec, contentHandler, tikaMetadata, parseContext);
       } catch {
-          case ex : Exception => ex.printStackTrace(System.err);
-          return None;
+        case ex : Throwable => {
+          logger.error("Error reading {}", rec.getHeader.getUrl, ex);
         }
       }
+      /* finish index */
+      rec.close;
+      indexContentHandler.contentString.map(str=>doc.addField(CONTENT_FIELD, str));
+
+      val title = tikaMetadata.get("title") match {
+        case s : String => s
+        case null => ""
+      }
+      updateDocBoost(doc, 1.0f);
+      updateDocUrlDigest(doc, url, rec.getDigestStr);
+      doc.addField(DATE_FIELD, recHeader.getDate.toLowerCase, 1.0f);
+      doc.addField(TYPE_FIELD, tikaMetadata.get(HttpHeaders.CONTENT_TYPE), 1.0f);
+      doc.addField(TITLE_FIELD, title, 1.0f);
+      doc.addField(CONTENT_LENGTH_FIELD, recHeader.getLength, 1.0f);
+      /* finish webgraph */
+      if (webGraphTypeRE.matcher(tikaMetadata.get(HttpHeaders.CONTENT_TYPE)).matches) {
+        val outlinks = wgContentHandler.outlinks;
+        if (outlinks.size > 0) {
+          val outlinkFps = for (l <- outlinks) 
+                           yield UriUtils.fingerprint(l.to);
+          for (fp <- outlinkFps.toList.distinct.sortWith((a,b)=>(a < b))) {
+            doc.addField("outlinks", fp);
+          }
+        }
+      }
+      return Some(doc);
+    } catch {
+      case ex : Exception => ex.printStackTrace(System.err);
+      return None;
     }
   }
 
--- src/main/scala/org/cdlib/was/ngIndexer/Utility.scala
+++ src/main/scala/org/cdlib/was/ngIndexer/Utility.scala
@@ -1,9 +1,23 @@
 package org.cdlib.was.ngIndexer;
 
-import org.archive.io.ArchiveRecord;
-import org.archive.io.arc.{ARCRecord,ARCReaderFactory};
+import org.apache.http.impl.io.AbstractSessionInputBuffer;
+import org.apache.http.message.ParserCursor;
+import org.apache.http.Header;
+import org.apache.http.ParseException;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.util.CharArrayBuffer;
+import org.apache.http.impl.io.{AbstractMessageParser,HttpResponseParser};
+import org.apache.http.impl.DefaultHttpResponseFactory;
 
-import java.io.File;
+import org.archive.io.{ArchiveReaderFactory,ArchiveRecord};
+import org.archive.io.arc.ARCRecord;
+import org.archive.io.warc.WARCRecord;
+
+import org.apache.http.message.BasicLineParser;
+
+import java.io.{BufferedInputStream,BufferedReader,File,InputStream,InputStreamReader};
+
+import scala.util.matching.Regex;
 
 object Utility {
   def skipHttpHeader (rec : ARCRecord) {
@@ -28,7 +42,7 @@ object Utility {
   }
 
   def eachArc (arcFile : java.io.File, f: (ArchiveRecord)=>Unit) {
-    val reader = ARCReaderFactory.get(arcFile)
+    val reader = ArchiveReaderFactory.get(arcFile)
     val it = reader.iterator;
     while (it.hasNext) {
       val next = it.next;
@@ -39,7 +53,7 @@ object Utility {
   }
 
   def eachArc (stream : java.io.InputStream, arcName : String, f: (ArchiveRecord)=>Unit) {
-    val reader = ARCReaderFactory.get(arcName, stream, true);
+    val reader = ArchiveReaderFactory.get(arcName, stream, true);
     val it = reader.iterator;
     while (it.hasNext) {
       val next = it.next;
@@ -48,4 +62,54 @@ object Utility {
     }
     reader.close;
   }
+
+  def readLine (br : BufferedReader) : CharArrayBuffer = {
+    val buff = new CharArrayBuffer(1024);
+    val line = br.readLine;
+    val arr = line.toCharArray;
+    buff.append(arr, 0, arr.length);
+    return buff;
+  }
+
+  def joinHeaderLines (lines : Seq[CharArrayBuffer]) : Seq[CharArrayBuffer] = {
+    var lastLine = new CharArrayBuffer(0);
+    var retval = List[CharArrayBuffer]();
+    for (line <- lines) {
+      if ((line.charAt(0) == ' ') || (line.charAt(0) == '\t')) {
+        lastLine.append(line);
+      } else {
+        retval = line :: retval;
+        lastLine = line;
+      }
+    }
+    return retval;
+  }
+
+  def readHeaderLines (br : BufferedReader) : Seq[CharArrayBuffer] = {
+    var lines = List[CharArrayBuffer]();
+    var line = readLine(br);
+    while (!line.isEmpty) {
+      lines = line :: lines;
+      line = readLine(br);
+    }
+    return joinHeaderLines(lines);
+  }
+
+  def parseHeaders (rec : WARCRecord) : Option[Pair[Int,Map[String,Header]]] = {
+    val header = rec.getHeader();
+    val br = new BufferedReader(new InputStreamReader(rec, "UTF-8"));
+    val firstLine = readLine(br);
+    val lineParser = new BasicLineParser;
+    try {
+      val statusLine = lineParser.parseStatusLine(firstLine, new ParserCursor(0, firstLine.length - 1));
+      val headers = readHeaderLines(br).map(lineParser.parseHeader(_));
+      var headerMap = Map[String,Header]();
+      headerMap ++= headers.map { (h)=> h.getName.toLowerCase->h };
+      return Some((statusLine.getStatusCode, headerMap));
+    } catch {
+      case ex : ParseException => {
+        return None;
+      }
+    }
+  } 
 }
