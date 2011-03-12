4701651291285996e711dbb5acef096d86a15ed5
Erik Hetzner <erik.hetzner@ucop.edu>
2011-03-11 22:30:38 -0800

    add & use ZkRetry trait

--- src/main/scala/org/cdlib/was/ngIndexer/Locker.scala
+++ src/main/scala/org/cdlib/was/ngIndexer/Locker.scala
@@ -14,30 +14,21 @@ import org.menagerie.{DefaultZkSessionManager,ZkSessionManager};
 import org.menagerie.locks.ReentrantZkLock;
 import org.menagerie.ZkUtils;
 
-class Locker (zkhosts : String) extends Retry {
-  private def mkLockDir (session : ZkSessionManager, path : String) {
-    ZkUtils.safeCreate(session.getZooKeeper, path, 
-                       Array[Byte](),
-                       Ids.OPEN_ACL_UNSAFE,
-                       CreateMode.PERSISTENT);
-  }
-
+class Locker (zkhosts : String) extends ZkRetry {
   def obtainLock[T] (lockName : String) (proc: => T) : T = {
     val session = new DefaultZkSessionManager(zkhosts, 10000);
-    mkLockDir(session, lockName);
     val l = new ReentrantZkLock(lockName, session);
-    l.lock;
+    retry { l.lock; }
     val retval = proc;
-    l.unlock;
+    retry { l.unlock; }
     session.closeSession;
     return retval;
-  }  
+  }
 
   def tryToObtainLock[T] (lockName : String) (proc: => T) (otherwise: => T) : T = {
     val session = new DefaultZkSessionManager(zkhosts, 10000);
-    mkLockDir(session, lockName);
     val l = new ReentrantZkLock(lockName, session);
-    val retval = if (l.tryLock) proc; else otherwise;
+    val retval = retry { if (l.tryLock) proc; else otherwise; }
     session.closeSession;
     return retval;
   }
---
+++ src/main/scala/org/cdlib/was/ngIndexer/ZkRetry.scala
@@ -0,0 +1,27 @@
+package org.cdlib.was.ngIndexer;
+
+import org.apache.zookeeper.KeeperException;
+
+trait ZkRetry {
+  /**
+   * Retry an operation a number of times, catch ZookeeperExceptions
+   * @param what What to do.
+   */
+  def retry[T] (what: => T) : T = {
+    var i = 0;
+    while (true) {
+      try {
+        val retval = what;
+        return what;
+      } catch {
+        case ex : KeeperException.ConnectionLossException => {
+          i = i + 1;
+          if (i >= 10) throw ex;
+        }
+        case ex : Throwable => throw ex;
+      }
+    }
+    /* should be unreachable */
+    return null.asInstanceOf[T];
+  }
+}
