ca5b6e929f61e7fb979f0f37cd4f7f4c9dce8ecf
Erik Hetzner <erik.hetzner@ucop.edu>
2011-04-08 10:55:08 -0700

    add dryrun command for testing

--- src/main/scala/org/cdlib/was/ngIndexer/SolrIndexer.scala
+++ src/main/scala/org/cdlib/was/ngIndexer/SolrIndexer.scala
@@ -2,6 +2,8 @@ package org.cdlib.was.ngIndexer;
 
 import java.io.{File,FileInputStream,FileNotFoundException,InputStream,IOException};
 
+import java.net.URI;
+
 import org.apache.solr.common.SolrInputDocument;
 
 import org.cdlib.ssconf.Configurator;
@@ -15,9 +17,13 @@ import org.cdlib.was.ngIndexer.SolrProcessor.{ARCNAME_FIELD,
 
 import org.cdlib.was.ngIndexer.SolrProcessor.{doc2InputDoc,mergeDocs,processStream,removeFieldValue};
 
+import scala.util.matching.Regex;
+
 /** Class used to index ARC files.
   */
 class SolrIndexer(config : Config) extends Retry with Logger {
+  val httpClient = new SimpleHttpClient;
+
   val server = new SolrDistributedServer(config.indexers(), 
                                          config.queueSize(), 
                                          config.queueRunners(),
@@ -82,6 +88,32 @@ class SolrIndexer(config : Config) extends Retry with Logger {
     return true;
   }
 
+  def dryrun (uri : URI, config : Config) {
+    val ArcRE = new Regex(""".*?([A-Za-z0-9\.-]+arc.gz).*""");
+    val ArcRE(arcName) = uri.getPath;
+    httpClient.getUri(uri) { (stream)=>
+      dryrun(stream, arcName, config);
+    }
+  }
+
+  def dryrun (file : File, config : Config) {
+    dryrun(new FileInputStream(file), file.getName, config);
+  }
+
+  def dryrun (stream : InputStream,
+              arcName : String,
+              config : Config) {
+    try {
+      processStream(arcName, stream, config) { (doc) =>
+        /* noop */
+      }
+    } catch {
+      case ex : Exception => {
+        logger.error("Exception while generating doc from arc ({}).", arcName, ex);
+      }
+    }
+  }
+
   def delete (file : File, removeFields : Map[String,String]) {
     Utility.eachArc(file) { 
       (rec) =>
@@ -122,52 +154,49 @@ object SolrIndexer {
     val config = loadConfigOrExit;
     val indexer = new SolrIndexer(config);
 
-    if (args.size < 3) {
-      System.err.println("Please supply >= 3 args!");
-      System.exit(1);
-    } else {
-      try {
-        val command = args(0);
-        val job = args(1);
-        val specification = args(2);
-        val project = args(3)
-        command match {
-          case "delete" => {
-            for (path <- args.drop(4)) {
-              indexer.delete(new File(path),
-                             Map(JOB_FIELD->job,
-                                 SPECIFICATION_FIELD->specification,
-                                 PROJECT_FIELD->project));
+    try {
+      val command = args(0);
+      command match {
+        case "dryrun" => 
+          for (path <- args.drop(1)) {
+            if (path.startsWith("http")) {
+              indexer.dryrun(new URI(path), config);
+            } else {
+              indexer.dryrun(new File(path), config);
             }
           }
-          case "index" => {
-            for (path <- args.drop(4)) {
-              indexer.index(new File(path), specification,
-                            Map(JOB_FIELD -> job, 
-                                SPECIFICATION_FIELD -> specification, 
-                                PROJECT_FIELD -> project),
-                            config)
+        case "delete" | "index" => {
+          val job = args(1);
+          val specification = args(2);
+          val project = args(3)
+          command match {
+            case "delete" => {
+              for (path <- args.drop(4)) {
+                indexer.delete(new File(path),
+                               Map(JOB_FIELD->job,
+                                   SPECIFICATION_FIELD->specification,
+                                   PROJECT_FIELD->project));
+              }
             }
-          }
-          case "test" => {
-            for (path <- args.drop(4)) {
-              indexer.index(new File(path), specification,
-                            Map(JOB_FIELD -> job, 
-                                SPECIFICATION_FIELD -> specification, 
-                                PROJECT_FIELD -> project),
-                            config)
+            case "index" => {
+              for (path <- args.drop(4)) {
+                indexer.index(new File(path), specification,
+                              Map(JOB_FIELD -> job, 
+                                    SPECIFICATION_FIELD -> specification, 
+                                  PROJECT_FIELD -> project),
+                              config)
+              }
             }
-          }
-
-          case _ => {
-            System.err.println("No command specified!");
-            System.exit(1);
           }
         }
-      } catch {
-        case ex : FileNotFoundException => ex.printStackTrace();
-        case ex : IOException => ex.printStackTrace();
+        case _ => {
+          System.err.println("No command specified!");
+          System.exit(1);
+        }
       }
+    } catch {
+      case ex : FileNotFoundException => ex.printStackTrace();
+      case ex : IOException => ex.printStackTrace();
     }
   }
 }
