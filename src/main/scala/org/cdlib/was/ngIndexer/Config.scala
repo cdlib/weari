4d8cf1ae061384efd67020b625c5212f5972a01a
Erik Hetzner <erik.hetzner@ucop.edu>
2011-05-27 14:12:24 -0700

    remove indexer config

--- src/main/scala/org/cdlib/was/ngIndexer/Config.scala
+++ src/main/scala/org/cdlib/was/ngIndexer/Config.scala
@@ -4,8 +4,6 @@ import org.cdlib.ssconf.SSConfig;
 import org.cdlib.ssconf.Value;
 
 trait Config extends SSConfig {
-  val indexers : Value[Seq[Tuple3[String,String,Int]]] = 
-    new Value[Seq[Tuple3[String,String,Int]]];
   val zooKeeperHosts  = new Value[String]();
   val zooKeeperPath   = new Value("/arcIndexQueue");
   val threadCount     = new Value(5);
