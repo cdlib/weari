d728e15fa6bfec2afdd10d0c6ea789b9fac0760a
Erik Hetzner <erik.hetzner@ucop.edu>
2011-06-09 12:57:21 -0700

    take a SolrServer arg

--- src/main/scala/org/cdlib/was/ngIndexer/SolrTermIterable.scala
+++ src/main/scala/org/cdlib/was/ngIndexer/SolrTermIterable.scala
@@ -1,13 +1,14 @@
 package org.cdlib.was.ngIndexer;
 
-import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.{SolrQuery,SolrServer};
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.params.SolrParams;
 
-class SolrTermIterable(val server : { def query (q : SolrParams) : QueryResponse; },
-                         val field : String)
-extends Iterable[String] {
-  
+class SolrTermIterable(server : SolrServer, field : String, queryString : String)
+  extends Iterable[String] {
+
+  def this(server : SolrServer, field : String) = this(server, field, "*:*");
+
   override def toString = 
     iterator.peek.map(el=>"(%s, ...)".format(el)).getOrElse("(empty");
 
@@ -15,7 +16,7 @@ extends Iterable[String] {
     var lowerLimit : String = "";
 
     def fillCache {
-      val q = new SolrQuery().setTerms(true).setTermsSortString("index").
+      val q = new SolrQuery(queryString).setTerms(true).setTermsSortString("index").
         addTermsField(field).setTermsLimit(100000).setQueryType("/terms").
         setTermsLower(lowerLimit);
       val results = server.query(q).getTermsResponse.getTerms(field);
