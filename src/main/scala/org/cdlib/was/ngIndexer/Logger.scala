31e7a1434602893d17e5ca7103fd3d048d002a68
Erik Hetzner <erik.hetzner@ucop.edu>
2011-09-23 15:55:38 -0700

    add catchAndLogExceptions

--- src/main/scala/org/cdlib/was/ngIndexer/Logger.scala
+++ src/main/scala/org/cdlib/was/ngIndexer/Logger.scala
@@ -1,7 +1,28 @@
 package org.cdlib.was.ngIndexer;
 
+import java.io.{PrintWriter,StringWriter};
+
 import org.slf4j.LoggerFactory;
 
-trait Logger { this : Object =>
-  val logger = LoggerFactory.getLogger(this.getClass);
+trait Logger { self =>
+
+  val logger = LoggerFactory.getLogger(self.getClass)
+
+  def getStackTrace(th : Throwable) : String = {
+    val result = new StringWriter;
+    val printWriter = new PrintWriter(result);
+    th.printStackTrace(printWriter);
+    return result.toString;
+  }
+
+  def catchAndLogExceptions (f: => Unit) {
+    try {
+      f;
+    } catch {
+      case ex : Exception => {
+        logger.error("Caught exception {}.", ex.toString);
+        logger.debug(getStackTrace(ex));
+      }
+    }
+  }
 }
