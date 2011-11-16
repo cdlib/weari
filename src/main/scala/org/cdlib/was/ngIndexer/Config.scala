622480bce0812dc72987e99d480ee30bbc3660a3
Erik Hetzner <erik.hetzner@ucop.edu>
2011-11-15 16:51:55 -0800

    remove zk stuff from config

--- src/main/scala/org/cdlib/was/ngIndexer/Config.scala
+++ src/main/scala/org/cdlib/was/ngIndexer/Config.scala
@@ -8,8 +8,6 @@ import org.cdlib.ssconf.SSConfig;
 import org.cdlib.ssconf.Value;
 
 trait Config extends SSConfig {
-  val zooKeeperHosts  = new Value[String]();
-  val zooKeeperPath   = new Value("/arcIndexQueue");
   val threadCount     = new Value(5);
   val queueSize       = new Value(1000);
   val queueRunners    = new Value(3);
