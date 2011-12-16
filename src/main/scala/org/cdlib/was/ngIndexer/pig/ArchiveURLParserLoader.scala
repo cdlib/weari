d0a69afbccf8c9b7cc849a6b7b461058be5a56bb
Erik Hetzner <erik.hetzner@ucop.edu>
2011-12-16 13:34:43 -0800

    rework loader

--- src/main/scala/org/cdlib/was/ngIndexer/pig/ArchiveURLParserLoader.scala
+++ src/main/scala/org/cdlib/was/ngIndexer/pig/ArchiveURLParserLoader.scala
@@ -1,9 +1,10 @@
 /* Copyright (c) 2011 The Regents of the University of California */
 
 
+
 package org.cdlib.was.ngIndexer.pig;
 
-import java.io.{File,FileInputStream};
+import java.io.{EOFException,File,FileInputStream};
 
 import java.security.{MessageDigest=>JavaMessageDigest};
 
@@ -21,7 +22,7 @@ import org.apache.pig.backend.hadoop.executionengine.mapReduceLayer.PigSplit;
 import org.apache.pig.data.{BagFactory,Tuple,TupleFactory};
 
 import org.cdlib.was.ngIndexer._;
-import org.cdlib.was.ngIndexer.Utility.{date2string,null2option};
+import org.cdlib.was.ngIndexer.Utility.{date2string,flushStream,null2option,withFileOutputStream};
 
 class ArchiveURLParserLoader extends LoadFunc with Logger {
   val tupleFactory = TupleFactory.getInstance();
@@ -34,102 +35,149 @@ class ArchiveURLParserLoader extends LoadFunc with Logger {
   var arcName : Option[String] = None;
   var tmpfile : Option[File] = None;
 
-  def setupNextArchiveReader : Boolean = {
-    if (!in.nextKeyValue()) {
-      return false;
+  def reset {
+    this.tmpfile.map(_.delete);
+    this.tmpfile = None;
+    this.it = None; 
+    this.arcName = None;
+  }
+    
+  /**
+   * Ensure that we have an iterator, or return false if we are done.
+   */
+  def ensureHasNext : Boolean = {
+    if (this.it.isDefined && this.it.get.hasNext) {
+      /* we already have an iterator! */
+      return true;
     } else {
-      /* clean up */
-      this.tmpfile.map(_.delete);
-      this.it = None; 
-      this.tmpfile = None;
-      this.arcName = None;
-
-      val value = this.in.getCurrentValue.asInstanceOf[Text]
-      val uri = new URI(value.toString);
-      val matcher = Utility.ARC_RE.pattern.matcher(uri.getPath);
-      if (!matcher.matches) {
-        return false;
-      } else {
-        this.arcName = Some(matcher.group(1));
-        indexer.httpClient.getUri(uri) {(is) =>
-          this.tmpfile = Some(new File(new File(System.getProperty("java.io.tmpdir")), this.arcName.getOrElse("")));
-          Utility.withFileOutputStream(this.tmpfile.get) { os =>
-            Utility.flushStream(is, os);
+      /* keep trying while there are more URIs to process */
+      while (in.nextKeyValue()) {
+        /* clean up */
+        reset;
+        
+        /* get a new ARC reader */
+        val value = this.in.getCurrentValue.asInstanceOf[Text]
+        val uri = new URI(value.toString);
+        val matcher = Utility.ARC_RE.pattern.matcher(uri.getPath);
+        if (!matcher.matches) {
+          /* this probably won't happen, but we should know about it if it does */
+          throw new Exception("Not an ARC file: %s".format(value));
+        } else {
+          this.arcName = Some(matcher.group(1));
+          /* download to a temp file with the arc name */
+          indexer.httpClient.getUri(uri) { is =>
+            this.tmpfile = Some(new File(new File(System.getProperty("java.io.tmpdir")), this.arcName.getOrElse("")));
+            withFileOutputStream(this.tmpfile.get) { os => flushStream(is, os) } 
           }
-          this.it = Some(ArchiveReaderFactoryWrapper.get(this.tmpfile.get).iterator);
+          if (this.tmpfile.isDefined) {
+            try {
+              this.it = Some(ArchiveReaderFactoryWrapper.get(this.tmpfile.get).iterator);
+            } catch {
+              case ex : EOFException => {
+                /* probably a completely empty file */
+                logger.error("Could not open arc: %s".format(this.arcName));
+              }
+            }
+          }
+          if (this.it.isDefined && this.it.get.hasNext) {
+            return true;
+          }
+          /* otherwise we keep trying */
         }
-        return this.it.isDefined;
+      }
+      /* no more URIs in list */
+      return false;
+    }
+  }
+
+  /**
+   * Checks if a gzip is valid. If it is, throw the exception,
+   * otherwise do not, and move to the next ARC.
+   */
+  def checkGzip (ex : Exception) {
+    if (this.tmpfile.isEmpty) {
+      throw ex;
+    } else {
+      if (Utility.checkGzip(this.tmpfile.get)) {
+        throw ex;
+      } else {
+        /* it is a bad gzip */
+        logger.error("Bad GZIP file: %s".format(this.arcName));
+        reset;
       }
     }
-  }      
+  }
 
-  def getNextArchiveRecord : Option[ParsedArchiveRecord] = {
-    var rec : Option[ArchiveRecordWrapper] = None;
+  /**
+   * Do something with the next record, closing the rec when finished.
+   * If we get an EOFException, check the gzip. If it is good, throw
+   * the exception. Otherwise just return the results of the function.
+   */
+  def withNextRecord[T] (f : (ArchiveRecordWrapper) => T) : Option[T] = {
+    var rec : ArchiveRecordWrapper = null;
     try {
-      while (rec.isEmpty) {
-        if (this.it.isEmpty || !this.it.get.hasNext) {
-          /* try to get a new ArchiveReader, otherwise return null */
-          if (!setupNextArchiveReader) {
-            return None;
-          }
-        } 
-        rec = Some(this.it.get.next);
-        if (!rec.get.isHttpResponse || rec.get.getStatusCode != 200) {
-          /* try again */
-            rec = None;
-        } else {
-          val retval = indexer.parseArchiveRecord(rec.get);
-          if (retval.isEmpty) {
-            /* this should not happen */
-            throw new Exception("Got empty parse: %s".format(rec.get.getUrl));
-          }
-          return retval;
-        }
-        try {
-          rec.map(_.close);
-          rec = None;
-        } catch {
-          case ex : Exception => {
-            logger.error("Caught exception CLOSING %s: {}".format(this.arcName.getOrElse("")));
-            throw ex;
-          }
-        }
-      }
+      rec = this.it.get.next;
+      return Some(f(rec));
     } catch {
       case ex : Exception => {
-        logger.error("Caught exception READING %s: {}".format(this.arcName.getOrElse("")));
-        throw ex;
+        /* log the exception before we rethrow it, so we know what arc was being read */
+        logger.error("Caught exception READING %s: {}".format(this.arcName.getOrElse("")), ex);
+        checkGzip(ex);
+        /* set to null so we don't try to close it later & throw ANOTHER exception */
+        rec = null;
+        return None;
+      }
+    } finally {
+      /* now try to close the record */
+      try {
+        if (rec != null) rec.close;
+      } catch {
+        case ex : Exception => {
+          /* log the exception before we rethrow it, so we know what arc was being closed */
+          logger.error("Caught exception CLOSING %s: {}".format(this.arcName.getOrElse("")), ex);
+          checkGzip(ex);
+        }
       }
     }
-    return None;
+  }
+
+  /**
+   * Transform a ParsedArchiveRecord into a Pig Tuple.
+   */
+  def rec2tuple (rec : ParsedArchiveRecord) : Tuple = {
+    var tuple = tupleFactory.newTupleNoCopy(new java.util.ArrayList[java.lang.Object]());
+    val outlinks = bagFactory.newDistinctBag;
+    for (link <- rec.outlinks) { 
+      outlinks.add(tupleFactory.newTuple(link));
+    }
+    for (value <- Seq(rec.getFilename,                // 0
+                      rec.getUrl,                     // 1\
+                      rec.getDigestStr.getOrElse(""), // 2
+                      date2string(rec.getDate),       // 3
+                      rec.getLength,                  // 4
+                      rec.content.getOrElse(""),      // 5
+                      rec.detectedContentType.        // 
+                        getOrElse(ContentType.DEFAULT).mediaType,
+                      rec.suppliedContentType.mediaType,  // 7
+                      rec.title.getOrElse(""),        // 8
+                      outlinks)) {                    // 9
+      tuple.append(value);
+    }
+    return tuple;
   }
   
   def getNext : Tuple = {
     try {
-      getNextArchiveRecord match {
-        case None => null;
-        case Some(rec) => {
-          var tuple = tupleFactory.newTupleNoCopy(new java.util.ArrayList[java.lang.Object]());
-          val outlinks = bagFactory.newDistinctBag;
-          for (link <- rec.outlinks) { 
-            outlinks.add(tupleFactory.newTuple(link));
-          }
-          for (value <- Seq(rec.getFilename,                // 0
-                            rec.getUrl,                     // 1
-                            rec.getDigestStr.getOrElse(""), // 2
-                            date2string(rec.getDate),       // 3
-                          rec.getLength,                  // 4
-                            rec.content.getOrElse(""),      // 5
-                            rec.detectedContentType.        // 
-                              getOrElse(ContentType.DEFAULT).mediaType,
-                            rec.suppliedContentType.mediaType,  // 7
-                            rec.title.getOrElse(""),        // 8
-                            outlinks)) {                    // 9
-            tuple.append(value);
-          }
-          tuple;
+      var next : Option[ParsedArchiveRecord] = None;
+      while (next.isEmpty) {
+        if (!ensureHasNext) {
+          return null;
+        } else {
+          /* we have Option[Option[ParsedArchiveRecord]] here, so we need to flatten */
+          next = withNextRecord(indexer.parseArchiveRecord(_)).flatten.headOption;
         }
       }
+      return rec2tuple(next.get);
     } catch {
       case ex : InterruptedException =>
         throw new ExecException("Error while reading input", 6018,
