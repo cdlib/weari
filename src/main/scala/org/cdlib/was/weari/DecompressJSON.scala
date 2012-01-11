eba32c11814ab3c4458cb46caf9e11632b692ee2
Erik Hetzner <erik.hetzner@ucop.edu>
2012-01-11 09:51:15 -0800

    catch exceptions when decompressing, allow user to pass in glob pattern

--- src/main/scala/org/cdlib/was/weari/DecompressJSON.scala
+++ src/main/scala/org/cdlib/was/weari/DecompressJSON.scala
@@ -10,24 +10,29 @@ import org.apache.hadoop.fs.Path;
 
 import org.cdlib.was.weari.Utility.{flushStream};
 
-object DecompressJSON {
-
-  val glob = new Path("/user/was/json/*.gz");
+/**
+ * Utility to decompress gz files in a Hadoop FS.
+ */
+object DecompressJSON extends Logger {
 
   def main (args : Array[String]) {
+    val glob = new Path(args(0));
     val conf = new Configuration();
     val fs = FileSystem.get(conf);
+
     for (childStatus <- fs.globStatus(glob)) {
       val path = childStatus.getPath;
       val name = path.getName;
       val newPath = new Path(path.getParent, name.substring(0, name.length-3));
       if (!fs.isFile(newPath)) {
         val in = fs.open(path);
-        val gzin = new GZIPInputStream(in);
-        val out = fs.create(newPath);
-        flushStream(gzin, out);
-        out.close;
-        gzin.close;
+        catchAndLogExceptions {
+          val gzin = new GZIPInputStream(in);
+          val out = fs.create(newPath);
+          flushStream(gzin, out);
+          out.close;
+          gzin.close;
+        }
       }
     }
   }
