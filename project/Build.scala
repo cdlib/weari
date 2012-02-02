39fc0defacddef5073961151c75de986d83a6a31
Erik Hetzner <erik.hetzner@ucop.edu>
2012-02-01 16:00:37 -0800

    add onejar config

--- project/Build.scala
+++ project/Build.scala
@@ -1,3 +1,4 @@
+import com.github.retronym.SbtOneJar;
 import java.io.File;
 import sbt._;
 import Keys._;
@@ -34,19 +35,28 @@ object MyBuild extends Build {
     version      := "0.1-DEV",
     scalaVersion := "2.9.1",
     exportJars   := true)
-                
-  lazy val root = Project("root",
-                          file("."),
-                          settings = buildSettings ++ Seq(distTask) ++ 
-                            Seq(name := "weari",
-                                distFiles := Seq(),
-                                distPath <<= (target) { (target) => target / "dist" / "artifacts.zip" },
-                                exportJars := true,
-                                externalPom(baseDirectory(_ / "pom.xml")),
-                                libraryDependencies ++= Seq("org.scalatest" %% "scalatest" % "1.6.1" % "test",
-                                                            "junit" % "junit" % "4.8.2" % "test"),
-                                resolvers := Seq("cdl-public" at "http://mvn.cdlib.org/content/repositories/public",
-                                                 "cdl-thirdparty" at "http://mvn.cdlib.org/content/repositories/thirdparty",
-                                                 "archive.org" at "http://builds.archive.org:8080/maven2/",
-                                                 "Local Maven Repository" at Path.userHome.asURL + "/.m2/repository")));
+
+  val extraResolvers = 
+    Seq("cdl-public" at "http://mvn.cdlib.org/content/repositories/public",
+        "cdl-thirdparty" at "http://mvn.cdlib.org/content/repositories/thirdparty",
+        "archive.org" at "http://builds.archive.org:8080/maven2/",
+        "Local Maven Repository" at Path.userHome.asURL + "/.m2/repository");
+
+  lazy val root = 
+    Project("root",
+            file("."),
+            settings = buildSettings ++ 
+              SbtOneJar.oneJarSettings ++
+              Seq(distTask) ++ 
+              Seq(name := "weari",
+                  distFiles := Seq(),
+                  distPath <<= (target) { (target) =>
+                    target / "dist" / "artifacts.zip" 
+                  },
+                  exportJars := true,
+                  externalPom(baseDirectory(_ / "pom.xml")),
+                  libraryDependencies ++= 
+                    Seq("org.scalatest" %% "scalatest" % "1.6.1" % "test",
+                        "junit" % "junit" % "4.8.2" % "test"),
+                  resolvers := extraResolvers));
 }
