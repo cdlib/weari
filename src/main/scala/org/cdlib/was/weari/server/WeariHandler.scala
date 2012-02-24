0fde476ec37f4d2035aaf25089a09e8d0eebec07
Erik Hetzner <erik.hetzner@ucop.edu>
2012-02-23 20:03:43 -0800

    first draft at starting pig parser from thrift server

--- src/main/scala/org/cdlib/was/weari/server/WeariHandler.scala
+++ src/main/scala/org/cdlib/was/weari/server/WeariHandler.scala
@@ -1,49 +1,49 @@
 package org.cdlib.was.weari.server;
 
-import org.cdlib.was.weari._;
-import org.cdlib.was.weari.thrift;
-
-import java.io.InputStream;
-import java.util.{ List => JList, Map => JMap };
-import java.util.zip.GZIPInputStream;
-
-import com.codahale.jerkson.Json.parse;
-
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.{ FileSystem, FSDataInputStream, FSDataOutputStream, Path };
-
-import org.apache.solr.client.solrj.impl.StreamingUpdateSolrServer;
-
-import scala.collection.JavaConversions.{ collectionAsScalaIterable, mapAsScalaMap };
-
+import org.cdlib.was.weari._
+import org.cdlib.was.weari.thrift
+import java.io.{InputStream, IOException}; 
+import java.util.{ List => JList, Map => JMap }
+import java.util.UUID;
+import java.util.zip.GZIPInputStream
+import com.codahale.jerkson.Json.parse
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{ FileSystem, FSDataInputStream, FSDataOutputStream, Path }
+import org.apache.solr.client.solrj.impl.StreamingUpdateSolrServer
+import scala.collection.JavaConversions.{ collectionAsScalaIterable, mapAsScalaMap, seqAsJavaList }
+import scala.collection.immutable.HashSet
 import grizzled.slf4j.Logging;
-
+import org.cdlib.was.weari.thrift.UnparsedException
+import org.apache.pig.PigServer;
+import org.cdlib.was.weari.Utility.null2option;
+    
 class WeariHandler(config: Config)
   extends thrift.Server.Iface with Logging {
 
   val conf = new Configuration();
   val fs = FileSystem.get(conf);
+  val jsonDir = new Path(config.jsonBaseDir());
 
   /**
    * Get the HDFS path for the JSON parse of an ARC file.
    * Can return either a .gz or a non-gzipped file.
    * 
    * @param arc The name of the ARC file.
-   * @return Some[Path] or None. 
+   * @return Path 
    */
-  def getPath(arc: String): Option[Path] = {
-    val json = "%s/%s.json".format(config.jsonBaseDir(), arc);
+  def getPath(arc: String): Path = {
+    val json = "%s.json".format(arc);
     val jsongz = "%s.gz".format(json);
-    val jsonPath = new Path(json);
+    val jsonPath = new Path(jsonDir, json);
     if (fs.exists(jsonPath)) {
-      return Some(jsonPath);
+      return jsonPath;
     } else {
-      val jsongzPath = new Path(jsongz);
+      val jsongzPath = new Path(jsonDir, jsongz);
       if (fs.exists(jsongzPath)) {
-        return Some(jsongzPath);
+        return jsongzPath;
       }
     }
-    return None;
+    throw new thrift.UnparsedException(arc);
   }
 
   /**
@@ -69,24 +69,19 @@ class WeariHandler(config: Config)
                                   filter = filter,
                                   extraId = extraId,
                                   extraFields = extraFields.toMap);
-    val arcPaths = arcs.toSeq.flatMap(getPath(_));
-    if (arcPaths.size != arcs.size) {
-      /* missing JSON files for some arcs */
-      throw new thrift.IndexException("Some ARCs not yet parsed.");
-    } else {
-      for ((path, arcname) <- arcPaths.zip(arcs)) {
-        var in : InputStream = null;
-        try {
-          in = fs.open(path);
-          if (path.getName.endsWith("gz")) {
-            in = new GZIPInputStream(in);
-          }
-          indexer.index(parse[List[ParsedArchiveRecord]](in));
-        } catch {
-          case ex : Exception => {
-            error("Error while indexing %s: %s".format(arcname, ex), ex);
-            if (in != null) in.close;
-          }
+
+    for ((arcname, path) <- arcs.zip(arcs.map(getPath(_)))) {
+      var in : InputStream = null;
+      try {
+        in = fs.open(path);
+    	if (path.getName.endsWith("gz")) {
+          in = new GZIPInputStream(in);
+        }
+        indexer.index(parse[List[ParsedArchiveRecord]](in));
+      } catch {
+        case ex : Exception => {
+          error("Error while indexing %s: %s".format(arcname, ex), ex);
+          if (in != null) in.close;
         }
       }
     }
@@ -95,4 +90,56 @@ class WeariHandler(config: Config)
   def ping {
     println("pinged");
   }
+  
+  def mkUUID : String = UUID.randomUUID().toString();
+
+  def mkArcList(arcs : Seq[String]) : Path = {
+    val arclistName = mkUUID;
+    val path = new Path(arclistName)
+    val os = fs.create(path);
+    for (arcname <- arcs) {
+      os.writeChars(arcname);
+      os.writeChar('\n');
+    }
+    os.close;
+    return path;
+  }
+
+  def refileJson (source : Path) {
+    fs.mkdirs(jsonDir);
+    null2option(fs.listStatus(source)).map { children =>
+      for (child <- children) {
+        val path = child.getPath;
+        if (child.isDir) {
+          refileJson(path);
+        } else {
+          if (path.getName.endsWith(".json") || path.getName.endsWith(".json.gz")) {
+            fs.rename(path, new Path(jsonDir, path.getName));
+          }
+        }
+      }
+    }
+  }
+
+  def parseArcs (arcs: JList[String]) {
+    val pigServer = new PigServer(org.apache.pig.ExecType.MAPREDUCE);
+    val arcListPath = mkArcList(arcs.toSeq);
+
+    /* add jars in classpath to registered jars in pig */
+    val cp = System.getProperty("java.class.path").split(System.getProperty("path.separator"));
+    for (entry <- cp if entry.endsWith("jar")) {
+      pigServer.registerJar(entry);
+    }
+
+    pigServer.registerQuery("""
+      Data = LOAD '%s' 
+      USING org.cdlib.was.weari.pig.ArchiveURLParserLoader()
+      AS (filename:chararray, url:chararray, digest:chararray, date:chararray, length:long, content:chararray, detectedMediaType:chararray, suppliedMediaType:chararray, title:chararray, outlinks);""".
+        format(arcListPath));
+      
+    pigServer.store("Data", "%s.json.gz".format(mkUUID),
+    		        "org.cdlib.was.weari.pig.JsonParsedArchiveRecordStorer");
+    		              
+  }	
 }
+
