27367d3044bc60d4379397ab152a73d612c37fb6
Erik Hetzner <erik.hetzner@ucop.edu>
2012-02-28 15:23:24 -0800

    Fix autoload.

--- lib/weari.rb
+++ lib/weari.rb
@@ -2,15 +2,5 @@
 # Copyright:: Copyright (c) 2011, Regents of the University of California
 
 module Weari
-  autoload :Builder, "weari/builder"
-  autoload :Parser,  "weari/parser"
-  autoload :Pig,     "weari/pig"
-  autoload :PigJob,  "weari/pigjob"
-
-  module Solr
-    autoload :CachingEnumerable,  "weari/solr/caching_enumerable"
-    autoload :DocumentEnumerable, "weari/solr/document_enumerable"
-    autoload :Indexer,            "weari/solr/indexer"
-    autoload :Merger,             "weari/solr/merger"
-  end
+  autoload :Client, "weari/client"
 end
