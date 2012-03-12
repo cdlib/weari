c2014cd48fb51b39577c28ac67c45761aa68321b
Erik Hetzner <erik.hetzner@ucop.edu>
2012-03-12 16:53:58 -0700

    remove junit stuff from BUild.scala

--- project/Build.scala
+++ project/Build.scala
@@ -53,8 +53,5 @@ object MyBuild extends Build {
                   },
                   exportJars := true,
                   externalPom(baseDirectory(_ / "pom.xml")),
-                  libraryDependencies ++= 
-                    Seq("org.scalatest" %% "scalatest" % "1.6.1" % "test",
-                        "junit" % "junit" % "4.8.2" % "test"),
                   resolvers := extraResolvers));
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -6,4 +6,3 @@ resolvers += Resolver.url(
   "sbt-plugin-releases", 
   new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases/")
 )(Resolver.ivyStylePatterns)
-
