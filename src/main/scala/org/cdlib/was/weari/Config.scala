e8ec32f64a544ea1649762745ca9fe0d5f0b1bd9
Erik Hetzner <erik.hetzner@ucop.edu>
2012-05-31 14:35:38 -0700

    remove unused config options

--- src/main/scala/org/cdlib/was/weari/Config.scala
+++ src/main/scala/org/cdlib/was/weari/Config.scala
@@ -11,7 +11,5 @@ trait Config extends SSConfig {
   val threadCount     = new Value(5);
   val queueSize       = new Value(1000);
   val queueRunners    = new Value(3);
-  val jsonCacheDir    = new Value(new File(System.getProperty("java.io.tmpdir")));
-  val arcServerBase   = new Value("http://localhost:54480/arcs/%s");
   val jsonBaseDir     = new Value("/user/was/json");
 }
