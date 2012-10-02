6b7c2ad3367a140a08d4dd8b5cbc36911028eb47
Erik Hetzner <erik.hetzner@ucop.edu>
2012-10-02 16:53:42 -0700

    add json test

---
+++ src/test/scala/org/cdlib/was/weari/tests/JsonTest.scala
@@ -0,0 +1,25 @@
+/* (c) 2009-2012 Regents of the University of California */
+
+package org.cdlib.was.weari.tests;
+
+import java.util.zip.GZIPInputStream;
+
+import org.cdlib.was.weari._;
+
+import org.scalatest.{FeatureSpec,GivenWhenThen,Ignore};
+
+class JsonTest extends FeatureSpec {
+  val cl = classOf[WarcSpec].getClassLoader;
+  val config = new Config {};
+  val jsonName = "test.warc.gz.json.gz";
+  val weari = new Weari(config);
+
+  feature ("JSON") {
+    scenario ("we can parse a JSON file") {
+      val records = weari.readJson("test.arc.gz", new GZIPInputStream(cl.getResourceAsStream(jsonName)));
+      assert(records.length === 1);
+      val record = records(0);
+      assert (record.url === "http://www.actorsequity.org/home.asp");
+    }
+  }
+}
