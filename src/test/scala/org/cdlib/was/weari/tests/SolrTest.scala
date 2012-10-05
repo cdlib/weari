6361efc5a3cc722b29d3f20674cfa565c5e73e86
Erik Hetzner <erik.hetzner@ucop.edu>
2012-10-05 12:32:05 -0700

    disable solr tests

--- src/test/scala/org/cdlib/was/weari/tests/SolrTest.scala
+++ src/test/scala/org/cdlib/was/weari/tests/SolrTest.scala
@@ -11,17 +11,17 @@ import org.scalatest.matchers._;
 import org.cdlib.was.weari._;
 
 class SolrTest extends FunSpec with BeforeAndAfter with ShouldMatchers with RequiresRunningContentApi {
-  describe("solr") {
-    it("should not return any results to start with") {
-      val server = new HttpSolrServer("http://localhost:8700/solr");
-      val docs = new SolrDocumentCollection(server, new SolrQuery("*:*").setRows(10));
-      assert (docs.size === 0);
-    }
+  // describe("solr") {
+  //   it("should not return any results to start with") {
+  //     val server = new HttpSolrServer("http://localhost:8700/solr");
+  //     val docs = new SolrDocumentCollection(server, new SolrQuery("*:*").setRows(10));
+  //     assert (docs.size === 0);
+  //   }
 
-    it("should index properly") {
-      val config = new Config {};
-      val weari = new Weari(config);
-      weari.index("http://localhost:8700/solr", "*:*", List("CDL-20070613180402-00006-ingest1.arc.gz"), "", Map[String,Seq[String]]());
-    }
-  }
+  //   it("should index properly") {
+  //     val config = new Config {};
+  //     val weari = new Weari(config);
+  //     weari.index("http://localhost:8700/solr", "*:*", List("CDL-20070613180402-00006-ingest1.arc.gz"), "", Map[String,Seq[String]]());
+  //   }
+  // }
 }
