5ecbfbbcc7c4e7df65df737ed13f700165c00bcd
Erik Hetzner <erik.hetzner@ucop.edu>
2012-11-01 11:42:13 -0700

    test WARC parsing

--- src/test/scala/org/cdlib/was/weari/tests/ParseTest.scala
+++ src/test/scala/org/cdlib/was/weari/tests/ParseTest.scala
@@ -4,22 +4,34 @@ package org.cdlib.was.weari.tests;
 
 import org.cdlib.was.weari._;
 
-import org.scalatest.GivenWhenThen;
+import org.scalatest.{ FeatureSpec, GivenWhenThen };
 
 import com.typesafe.config.ConfigFactory;
 
 class ParseTest extends FeatureSpec {
   val cl = classOf[ParseTest].getClassLoader;
   val config = new Config(ConfigFactory.load("test"));
-  val arcName = "IAH-20080430204825-00000-blackbook.arc.gz";
+  val arcname = "IAH-20080430204825-00000-blackbook.arc.gz";
+  var arcpath = cl.getResource(arcname).toString;
+  val warcname = "IAH-20080430204825-00000-blackbook.warc.gz";
+  var warcpath = cl.getResource(warcname).toString;
+  val w = new Weari(config);
 
   feature ("pig parser") {
     scenario ("can parse an ARC file") {
-      val w = new Weari(config);
-      w.deleteParse(arcName);
-      w.parseArcs (List(cl.getResource(arcName).toString));
-      assert (w.isArcParsed(arcName));
-      val recs = w.pigUtil.readJson(w.pigUtil.getPath(arcName));
+      w.deleteParse(arcname);
+      w.parseArcs(List(arcpath))
+      assert (w.isArcParsed(arcname));
+      val recs = w.pigUtil.readJson(w.pigUtil.getPath(arcname));
+      assert (recs(0).url === "http://www.archive.org/robots.txt");
+      assert (recs(0).detectedContentType === Some(ContentType("text", "plain", None)));
+    }
+
+    scenario ("can parse a WARC file") {
+      w.deleteParse(warcname);
+      w.parseArcs(List(warcpath));
+      assert (w.isArcParsed(warcname));
+      val recs = w.pigUtil.readJson(w.pigUtil.getPath(arcname));
       assert (recs(0).url === "http://www.archive.org/robots.txt");
       assert (recs(0).detectedContentType === Some(ContentType("text", "plain", None)));
     }
