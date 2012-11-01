2652a86bedef3b930cad7c8de974f65969e34341
Erik Hetzner <erik.hetzner@ucop.edu>
2012-11-01 11:41:51 -0700

    working solr tests

--- src/test/scala/org/cdlib/was/weari/tests/SolrTest.scala
+++ src/test/scala/org/cdlib/was/weari/tests/SolrTest.scala
@@ -10,12 +10,39 @@ import org.scalatest.matchers._;
 
 import org.cdlib.was.weari._;
 
-class SolrTest extends FunSpec with BeforeAndAfter with ShouldMatchers {
+import com.typesafe.config.ConfigFactory;
+
+class SolrTest extends FunSpec with ShouldMatchers {
+  val cl = classOf[ParseTest].getClassLoader;
+  val config = new Config(ConfigFactory.load("test"));
+  val w = new Weari(config);
+  val arcname = "IAH-20080430204825-00000-blackbook.arc.gz";
+  var arcpath = cl.getResource(arcname).toString;
+  val server = new HttpSolrServer("http://localhost:8983/solr");
+
   describe("solr") {
     it("should not return any results to start with") {
-      val server = new HttpSolrServer("http://localhost:8983/solr");
       val docs = new SolrDocumentCollection(server, new SolrQuery("*:*").setRows(10));
       assert (docs.size === 0);
+    }
+    
+    it("should index properly") {
+      val config = new Config;
+      if (!w.isArcParsed(arcname)) {
+        w.parseArcs(List(arcpath));
+      }
+      w.index("http://localhost:8983/solr", "*:*", 
+              List(arcname), "", Map[String,Seq[String]]());
+    }
+    
+    it("should have indexed images") {
+      val docs = new SolrDocumentCollection(server, new SolrQuery("mediatypegroupdet:image").setRows(10));
+      assert (docs.size === 55);
+    }
+
+    it("should be able to remove arcs") {
+      w.remove("http://localhost:8983/solr", List(arcname));
+      assert (0 === new SolrDocumentCollection(server, new SolrQuery("*:*")).size);
     }
   }
 }
