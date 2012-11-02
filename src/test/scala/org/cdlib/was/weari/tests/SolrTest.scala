12ba8417da037a3e9a7309b123f756a86e987cb2
Erik Hetzner <erik.hetzner@ucop.edu>
2012-11-02 16:09:39 -0700

    test index threading

--- src/test/scala/org/cdlib/was/weari/tests/SolrTest.scala
+++ src/test/scala/org/cdlib/was/weari/tests/SolrTest.scala
@@ -1,5 +1,6 @@
 /* (c) 2009-2010 Regents of the University of California */
 
+
 package org.cdlib.was.weari.tests;
 
 import org.apache.solr.client.solrj.SolrQuery;
@@ -12,7 +13,7 @@ import org.cdlib.was.weari._;
 
 import com.typesafe.config.ConfigFactory;
 
-class SolrTest extends FunSpec with ShouldMatchers {
+class SolrTest extends FunSpec with ShouldMatchers with BeforeAndAfter {
   val cl = classOf[ParseTest].getClassLoader;
   val config = new Config(ConfigFactory.load("test"));
   val w = new Weari(config);
@@ -36,19 +37,19 @@ class SolrTest extends FunSpec with ShouldMatchers {
       if (!w.isArcParsed(arcname)) {
         w.parseArcs(List(arcpath));
       }
-      w.index(solrurl, "*:*", List(arcname), "", Map[String,Seq[String]]());
+      w.index(solrurl, "*:*", List(arcname), "");
     }
 
     it("reindexing should work") {
       val docsa = mkSearch("*:*").toList.sortBy(_.getFirstValue("id").asInstanceOf[String])
-      w.index(solrurl, "*:*", List(arcname), "", Map[String,Seq[String]]());
+      w.index(solrurl, "*:*", List(arcname), "");
       val docsb = mkSearch("*:*").toList.sortBy(_.getFirstValue("id").asInstanceOf[String])
       for ((a, b) <- docsa.zip(docsb);
            field <- List("id", "date", "content", "url", "canonicalurl", "title", "mediatypedet", "mediatypesup")) {
              assert (a.getFieldValue(field) === b.getFieldValue(field));
            }
     }
-    
+
     it("should have indexed images") {
       assertSearchSize("mediatypegroupdet:image", 55);
     }
@@ -64,6 +65,47 @@ class SolrTest extends FunSpec with ShouldMatchers {
     it("should be able to remove arcs") {
       w.remove(solrurl, List(arcname));
       assertSearchSize("*:*", 0);
+    }
+
+    it("threaded indexing should work") {
+      val threads = for (i <- (1 to 10))
+                    yield {
+                      val t = new Thread {
+                        val is = i.toString;
+                        override def run {
+                          w.index(solrurl, "job:%s".format(is), List(arcname), is, Map("job"->List(is)));
+                        }
+                      }
+                      t.start;
+                      t;
+                    }
+      threads.map(_.join);
+      
+      for (i <- (1 to 10)) {
+        assertSearchSize("job:%s".format(i.toString), 214);
+      }
+      assertSearchSize("*:*", 214 * 10);
+      w.remove(solrurl, List(arcname));
+    }
+
+    it("threaded indexing should lock on the same extraId") {
+      val threads = for (i <- (1 to 2))
+                    yield {
+                      val t = new Thread {
+                        val is = i.toString;
+                        override def run {
+                          w.index(solrurl, "job:%s".format(is), List(arcname), "XXX");
+                        }
+                      }
+                      t;
+                    }
+      assert(w.isLocked("XXX") === false);
+      threads.map(_.start);
+      /* this depends on the threads not being finished, which should be true */
+      assert(w.isLocked("XXX") === true);
+      threads.map(_.join);
+      assert(w.isLocked("XXX") === false);
+      w.remove(solrurl, List(arcname));
     }
   }
 }
