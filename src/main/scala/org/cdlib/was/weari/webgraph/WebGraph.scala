0207cb84350691c8d7ef21aa9381e3113aa9f5b6
Erik Hetzner <erik.hetzner@ucop.edu>
2013-08-14 18:37:36 -0700

    removing it.unimi webgraph stuff for now
    --HG--
    rename : src/main/java/org/cdlib/was/weari/webgraph/MyImmutableSequentialGraph.java => src/main/java/org/cdlib/was/weari/webgraph/MyImmutableSequentialGraph.java.off
    rename : src/main/scala/org/cdlib/was/weari/webgraph/RankedWebGraph.scala => src/main/scala/org/cdlib/was/weari/webgraph/RankedWebGraph.scala.off
    rename : src/main/scala/org/cdlib/was/weari/webgraph/SolrWebGraph.scala => src/main/scala/org/cdlib/was/weari/webgraph/SolrWebGraph.scala.off

--- src/main/scala/org/cdlib/was/weari/webgraph/WebGraph.scala
+++ src/main/scala/org/cdlib/was/weari/webgraph/WebGraph.scala
@@ -38,7 +38,7 @@ import scala.collection.mutable.HashMap;
 abstract class WebGraph {
   def addLink (link : Outlink);
   def addLinks (links : Seq[Outlink]);
-  def nodeIterator : it.unimi.dsi.webgraph.NodeIterator;
+//  def nodeIterator : it.unimi.dsi.webgraph.NodeIterator;
   def numNodes : Int;
 
   var knownUrlCounter = 0;
@@ -56,28 +56,28 @@ abstract class WebGraph {
   }
 }
 
-object WebGraph {
-  def check (wg : WebGraph) {
-    val g = new MyImmutableSequentialGraph(wg);
-    val it = g.nodeIterator;
-    var i = 0;
-    while (it.hasNext) {
-      val u = it.next;
-      if (u != i) throw new RuntimeException("node seq error %d != %d.".format(i, u));
-      i = i + 1;
-    }
-    val numNodes = g.numNodes;
-    if (numNodes != i) throw new RuntimeException("num nodes error.");
-    val it2 = g.nodeIterator;
-    while (it2.hasNext) {
-      it2.next;
-      if (it.outdegree > 0) {
-        val a = it.successorArray;
-        if (a.length != it.outdegree) { System.err.println("Bad outdegree.") };
-        for (j <- a) {
-          if (j > numNodes) { System.err.println("Bad successor %d.".format(j)); }
-        }
-      }
-    }
-  }
-}
+// object WebGraph {
+//   def check (wg : WebGraph) {
+//     val g = new MyImmutableSequentialGraph(wg);
+//     val it = g.nodeIterator;
+//     var i = 0;
+//     while (it.hasNext) {
+//       val u = it.next;
+//       if (u != i) throw new RuntimeException("node seq error %d != %d.".format(i, u));
+//       i = i + 1;
+//     }
+//     val numNodes = g.numNodes;
+//     if (numNodes != i) throw new RuntimeException("num nodes error.");
+//     val it2 = g.nodeIterator;
+//     while (it2.hasNext) {
+//       it2.next;
+//       if (it.outdegree > 0) {
+//         val a = it.successorArray;
+//         if (a.length != it.outdegree) { System.err.println("Bad outdegree.") };
+//         for (j <- a) {
+//           if (j > numNodes) { System.err.println("Bad successor %d.".format(j)); }
+//         }
+//       }
+//     }
+//   }
+// }
