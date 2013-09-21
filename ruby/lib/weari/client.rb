9dd75466974e3ea0d7c7493edaf432819a90072a
Erik Hetzner <erik.hetzner@ucop.edu>
2013-09-20 18:13:26 -0700

    fix ruby for new server

--- ruby/lib/weari/client.rb
+++ ruby/lib/weari/client.rb
@@ -1,43 +1,11 @@
-require 'rsolr'
 require 'weari/thrift/server'
 
 module Weari
   class Client
-    # shuffle an arraye
-    def shuffle(a_orig)
-      a = a_orig.dup
-      n = a.size
-      while (n > 1) do
-        k = rand(n)
-        n = n - 1
-        old = a[n]
-        a[n] = a[k]
-        a[k] = old
-      end
-      return a
-    end
-
     def initialize(host, port)
       @t_transport = ::Thrift::BufferedTransport.new(::Thrift::Socket.new(host, port))
       @t_protocol = ::Thrift::BinaryProtocol.new(@t_transport)
       @t_client = Weari::Thrift::Server::Client.new(@t_protocol)
-    end
-
-    # Return the least used solr server from a list
-    def get_best_solr_server(servers)
-      best_server = nil
-      best_server_doccount = 0
-      shuffle(servers).each do |url|
-        rsolr = RSolr.connect(:url=>url)
-        response = rsolr.get('select', 
-                             :params=>{"q" => '*:*', "wt" => "ruby", "rows"=>0})
-        doccount = response["response"]["numFound"]
-        if (best_server.nil? || (doccount < best_server_doccount)) then
-          best_server = url
-          best_server_doccount = doccount
-        end
-      end
-      return best_server
     end
 
     def with_open_transport
@@ -52,34 +20,22 @@ module Weari
         @t_client.parseArcs(arcs)
       end
     end
-
-    def parse_arc(arc)
-      return with_open_transport do
-        @t_client.parseArc(arc)
-      end
-    end
     
-    def index(solr_uri, filter, arcs, extra_id, extra_fields)
+    def index(arcs, extra_id, extra_fields)
       with_open_transport do
-        @t_client.index(solr_uri, filter, arcs, extra_id, extra_fields)
+        @t_client.index(arcs, extra_id, extra_fields)
       end
     end
 
-    def remove(solr_uri, arcs)
+    def remove(arcs)
       with_open_transport do
-        @t_client.remove(solr_uri, arcs)
+        @t_client.remove(arcs)
       end
     end
 
-    def move(query, from, to)
+    def set_fields(query, fields)
       with_open_transport do
-        @t_client.move(query, from, to)
-      end
-    end
-
-    def set_fields(solr_uri, query, fields)
-      with_open_transport do
-        @t_client.setFields(solr_uri, query, fields)
+        @t_client.setFields(query, fields)
       end
     end
 
@@ -98,12 +54,6 @@ module Weari
     def delete_parse(arc)
       return with_open_transport do
         @t_client.deleteParse(arc)
-      end
-    end
-
-    def clear_merge_manager(manager_id)
-      return with_open_transport do
-        @t_client.clearMergeManager(manager_id)
       end
     end
   end
